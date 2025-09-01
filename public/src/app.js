b06899a12f63cd014c5824f25717bd424b255b96
Cyber820 <jieyangpan@protonmail.com>
2025-09-01 14:55:23 +0800

    Update app.js

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,9 @@
-console.log('app.js loaded');
+// src/app.js
+import { attachEventDataAttrs, applyStyleState } from './style/engine.js';
+import { getStyleState, setStyleState, onStyleStateChange } from './state/styleState.js';
+
+// 暴露给现有内联代码使用（避免你现在就重写那段脚本）
+window.__styleEngine = { attachEventDataAttrs, applyStyleState };
+window.__styleState  = { getStyleState, setStyleState, onStyleStateChange };
+
+console.log('app.js loaded (style engine + state ready)');
