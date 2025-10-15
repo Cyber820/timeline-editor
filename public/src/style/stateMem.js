de2d8cb2d4746575d2666da2cd39c30946b36def
Cyber820 <jieyangpan@protonmail.com>
2025-10-15 14:34:55 +0800

    Update stateMem.js

--- public/src/style/stateMem.js
+++ public/src/style/stateMem.js
@@ -1,1 +1,7 @@
-
+export const stateMem = {
+  currentStyleAttr: null,
+  boundStyleType: {},       // { [attrKey]: 'fontColor' | 'borderColor' | 'backgroundColor' | 'fontFamily' | 'haloColor' | 'none' }
+  styleTypeOwner: {},       // { [styleKey]: attrKey }
+  styleRules: {},           // { [attrKey]: Array<{ id, type, style: {}, values: string[] }> }
+  styleRowSelections: {},   // { [rowId]: string[] }
+};
