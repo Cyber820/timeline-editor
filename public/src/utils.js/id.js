031387789e187fceb0f83903db2b2771d1d89f07
Cyber820 <jieyangpan@protonmail.com>
2025-10-17 15:08:18 +0800

    Delete public/src/utils.js directory

--- public/src/utils.js/id.js
+++
@@ -1,5 +0,0 @@
-export function genId() {
-  return (typeof crypto !== 'undefined' && crypto.randomUUID)
-    ? crypto.randomUUID()
-    : ('r_' + Date.now() + '_' + Math.random().toString(36).slice(2));
-}
