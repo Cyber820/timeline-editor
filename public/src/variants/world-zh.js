1a57e4ff78d0ba9ad4f3e448b029b342a8a6a0a7
Cyber820 <jieyangpan@protonmail.com>
2025-12-16 14:51:46 +0800

    Create world-zh.js

---
+++ public/src/variants/world-zh.js
@@ -0,0 +1,7 @@
+export default {
+  key: 'world-zh',
+  title: '电子游戏时间轴Beta版本',
+  endpoint: 'https://script.google.com/macros/s/【你当前中文版接口】/exec',
+  locale: 'zh',
+  strings: {}, // 先不做 UI 英文化，这里留空
+};
