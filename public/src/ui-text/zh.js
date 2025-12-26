a1993a6db45c8c0aed08344b6e7875ca49fa9134
Cyber820 <jieyangpan@protonmail.com>
2025-12-26 14:25:00 +0800

    Update zh.js

--- public/src/ui-text/zh.js
+++ public/src/ui-text/zh.js
@@ -1,1 +1,31 @@
+// public/src/ui-text/zh.js
+export default {
+  info: {
+    buttons: {
+      usage: '使用方法',
+      roadmap: '开发计划',
+      feedback: '反馈',
+    },
+    dialogs: {
+      usageTitle: '使用方法',
+      roadmapTitle: '开发计划和反馈',
+      feedbackTitle: '反馈与建议',
+      intro:
+        '如果你在时间轴中发现了错误、遗漏，或者有补充的资料与建议，欢迎在这里填写。\n个人 ID 可以是你的昵称、常用 ID，方便后续在版本日志中致谢。',
+    },
+    form: {
+      idLabel: '个人 ID（必填）',
+      idRequiredAlert: '请填写“个人 ID”（可以是昵称、编号等）。',
+      contactLabel: '联系方式（选填）',
+      contactPlaceholder: '邮箱 / QQ / 社交账号（可留空）',
+      contentLabel: '反馈内容（必填）',
+      contentPlaceholder: '请描述你想反馈的问题、建议或补充信息',
+      contentRequiredAlert: '请填写反馈内容。',
+      submit: '提交',
+      cancel: '取消',
+      okToast: '感谢你的反馈！信息已经发送到维护者的反馈文档。',
+      failToast: '提交时出现了一些问题，可以稍后再试，或通过其他方式联系维护者。',
+    },
+  },
+};
 
